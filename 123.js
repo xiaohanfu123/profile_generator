68814ac31d6226fbfe102325471ae5b44f77c298
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-31 19:19:48 +0000

    ask

--- 123.js
+++ 123.js
@@ -1,12 +1,27 @@
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+////////////
+// Event Handling for User Input
+////////////
+
 // on any input from stdin (standard input), output a "." to stdout
-
-const readline = require('readline')
-
-process.stdin.on('data', (data) => {
+stdin.on('data', (key) => {
   process.stdout.write('.');
-  if (data === '\u0003') {
+  if (key === '\u0003') {
     process.exit();
   }
 });
 
 console.log('after callback');
+
+rl.question("What's your name?", (answer) => {
+  rl.question("What's an activity you like doing?",(name) => {  
+   rl.question('What do you listen to while doing that?') ,(music)=>{
+     rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)')
+       rl.question("What's your favourite thing to eat for that meal?")
+         rl.question('Which sport is your absolute favourite?')
+         rl.question('What is your superpower? In a few words, tell us what you are amazing at!') 
+   rl.close();
--- survey.js
+++ survey.js
@@ -5,8 +5,42 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What's your name?", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
+  rl.question("What's your name?", (name) => {
+  rl.question("What's an activity you like doing??", (activity) => {
+  console.log(`Your name is : ${name}`);
+  console.log(`you like to play ${activity}`)
   rl.close();
-});
+})});
+
+
+
+
+// What's your name? Nicknames are also acceptable :)
+// What's an activity you like doing?
+// What do you listen to while doing that?
+// Which meal is your favourite (eg: dinner, brunch, etc.)
+// What's your favourite thing to eat for that meal?
+// Which sport is your absolute favourite?
+// What is your superpower? In a few words, tell us what you are amazing at!
+
+
+// function input(prompt) {
+//   return new Promise((callbackFn, errorFn) => {
+//       rli.question(prompt, (uinput)=> {
+//           callbackFn(uinput);
+//       }, ()=> {
+//           errorFn();
+//       });
+//   });
+// }
+
+
+// const main = async () => {
+//   name = await input("what is your name? ");
+//   console.log(name);
+//   answer = await input(" what do you like ");
+//   console.log(answer);
+//   rli.close();
+// };
+
+// main()
